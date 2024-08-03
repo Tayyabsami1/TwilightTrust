39de94fb06799458ec4e1f458cbdac4ebfe4ef0e
Tayyab Kamran Sami <115224619+Tayyabsami1@users.noreply.github.com>
2024-08-03 15:43:10 +0500

    Login, SignUp page UI

--- Frontend/src/Pages/Login.jsx
+++ Frontend/src/Pages/Login.jsx
@@ -1,8 +1,91 @@
+import { Link } from 'react-router-dom';
+import '../Styles/Login.scss'
+import BgImg from "../assets/login_bg.jpg"
+import { useNavigate } from 'react-router-dom';
 
+import { useState } from 'react';
 
 const Login = () => {
+
+  const navigate=useNavigate();
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+    userType: 'Individual' // Default value
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    navigate('/');
+    // API call to Login user here
+  };
+
   return (
-    <div>Login</div>
+    <div className="LogIn">
+      <div className="left">
+        <div className="wrapper">
+          <h1>Welcome Back! <span>Connect </span>and Trade with Confidence. </h1>
+          <form onSubmit={handleSubmit}>
+
+            <div>
+              <label htmlFor="username">Username:</label>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                value={formData.username}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            <div>
+              <label htmlFor="password">Password:</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            <div>
+              <label htmlFor="userType">Log in as:</label>
+              <select
+                id="userType"
+                name="userType"
+                value={formData.userType}
+                onChange={handleChange}
+              >
+                <option value="Buyer">Buyer</option>
+                <option value="Seller">Seller</option>
+              </select>
+            </div>
+
+            <div className="btns">
+              <button type="submit">Log in </button>
+              <Link to='/SignUp'>Don't have an account? Sign up </Link>
+            </div>
+
+          </form>
+        </div>
+      </div>
+
+
+
+      <div className="right">
+      <img src={BgImg} alt="" />
+
+      </div>
+    </div>
   )
 }
 
--- Frontend/src/Pages/Signup.jsx
+++ Frontend/src/Pages/Signup.jsx
@@ -1,9 +1,118 @@
+import { Link } from 'react-router-dom';
+import '../Styles/Signup.scss'
+import BgImg from "../assets/signUp_bg.jpg"
 
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const Signup = () => {
+const SignUp = () => {
+
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    username: '',
+    phoneNumber: '',
+    email: '',
+    password: '',
+    userType: 'Individual' // Default value
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    // API call to register user here
+    navigate('/');
+  };
+
   return (
-    <div>Signup</div>
+    <div className="SignIn">
+      <div className="left">
+        <img src={BgImg} alt="" />
+      </div>
+    
+
+
+      <div className="right">
+        <div className="wrapper">
+        <h1>Join Our <span>Marketplace</span>- Connect, Buy, Sell Securely</h1>
+        <form onSubmit={handleSubmit}>
+
+          <div>
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div>
+            <label htmlFor="phoneNumber">Phone Number:</label>
+            <input
+              type="number"
+              id="phoneNumber"
+              name="phoneNumber"
+              value={formData.phoneNumber}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div>
+            <label htmlFor="userType">Sign in as:</label>
+            <select
+              id="userType"
+              name="userType"
+              value={formData.userType}
+              onChange={handleChange}
+            >
+              <option value="Buyer">Buyer</option>
+              <option value="Seller">Seller</option>
+            </select>
+          </div>
+
+          <div className="btns">
+            <button type="submit">Sign Up</button>
+            <Link to='/Login'>Already have an account? Login</Link>
+          </div>
+
+        </form>
+      </div>
+    </div>
+    </div>
   )
 }
 
-export default Signup
+export default SignUp
